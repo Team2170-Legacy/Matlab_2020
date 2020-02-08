a12094671080490dd90dbbee34cdf5a94c8803ed
Walson Li <46850531+Walson124@users.noreply.github.com>
2020-02-08 10:03:41 -0500

    Made draw_ball variables safer to use, set to pwrCell. + var

--- Autonomy/draw_Balls.m
+++ Autonomy/draw_Balls.m
@@ -1,41 +1,45 @@
-r = 3.5*in;
+pwrCell.r = 3.5*in;
 
 %red trench run first 3 balls (from left)
-x = 20.5*ft;
-y = 24.6875*ft;
-th = 0:pi/100:2*pi;
-a = r*cos(th) + x;
-b = r*sin(th) + y;
+pwrCell.x = 20.5*ft;
+pwrCell.y = 24.6875*ft;
+pwrCell.th = 0:pi/100:2*pi;
+pwrCell.a = pwrCell.r*cos(pwrCell.th) + pwrCell.x;
+pwrCell.b = pwrCell.r*sin(pwrCell.th) + pwrCell.y;
 for ball_i = 1:3
-    circ = plot(a,b,'y','linewidth', 6);
-    a = a + 3*ft;
+    plot(pwrCell.a,pwrCell.b,'y','linewidth', 6);
+    pwrCell.a = pwrCell.a + 3*ft;
 end
 %red trench double balls 1.7291665
-x = 32.2891665*ft;
-y = 25.4583335*ft;
+pwrCell.x = 32.2891665*ft;
+pwrCell.y = 25.4583335*ft;
 for ball_i = 1:2
-    a = r*cos(th) + x;
-    b = r*sin(th) + y;
-    circ = plot(a,b,'y','linewidth', 6);
-    y = 23.9166665*ft;
+    pwrCell.a = pwrCell.r*cos(pwrCell.th) + pwrCell.x;
+    pwrCell.b = pwrCell.r*sin(pwrCell.th) + pwrCell.y;
+    plot(pwrCell.a,pwrCell.b,'y','linewidth', 6);
+    pwrCell.y = 23.9166665*ft;
 end
 
 %blue trench run first 3 balls (from left)
-x = 32.5*ft;
-y = 2.3125*ft;
-th = 0:pi/100:2*pi;
-a = r*cos(th) + x;
-b = r*sin(th) + y;
+pwrCell.x = 32.5*ft;
+pwrCell.y = 2.3125*ft;
+pwrCell.th = 0:pi/100:2*pi;
+pwrCell.a = pwrCell.r*cos(pwrCell.th) + pwrCell.x;
+pwrCell.b = pwrCell.r*sin(pwrCell.th) + pwrCell.y;
 for ball_i = 1:3
-    circ = plot(a,b,'y','linewidth', 6);
-    a = a - 3*ft;
+    plot(pwrCell.a,pwrCell.b,'y','linewidth', 6);
+    pwrCell.a = pwrCell.a - 3*ft;
 end
 %blue trench double balls 1.7291665
-x = 20.71*ft;
-y = 1.5416665*ft;
+pwrCell.x = 20.71*ft;
+pwrCell.y = 1.5416665*ft;
 for ball_i = 1:2
-    a = r*cos(th) + x;
-    b = r*sin(th) + y;
-    circ = plot(a,b,'y','linewidth', 6);
-    y = 3.0833335*ft;
+    pwrCell.a = pwrCell.r*cos(pwrCell.th) + pwrCell.x;
+    pwrCell.b = pwrCell.r*sin(pwrCell.th) + pwrCell.y;
+    plot(pwrCell.a,pwrCell.b,'y','linewidth', 6);
+    pwrCell.y = 3.0833335*ft;
 end
+
+%SG Balls: 6 balls each short side, 8 balls each long side
+%Total can only be 5 balls per alliance in SG so...
+%Ball spacing is 1ft, 4.5in
