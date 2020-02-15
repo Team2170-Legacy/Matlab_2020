64ad9489e768b596d5794322dc144ce4ce4caf11
Priyanshu4 <agpri123@gmail.com>
2020-02-15 10:42:53 -0500

    Added actual applied output voltage to datalogging and plotting

--- DriveTrain_DataLogging/plotDriveTrainData.m
+++ DriveTrain_DataLogging/plotDriveTrainData.m
@@ -1,16 +1,29 @@
 % plotData.m
 % Written by Jacob K and Priyanshu on 2/8/2020
 %% Load in data
-data = GetDriveTrainLogTable('DriveTrainLog_2020-02-14_19_05.csv', [2, Inf]);
+data = GetDriveTrainLogTable_v2('DriveTrainLog_2020-02-15_10_01.csv', [2, Inf]);
 time = data.Time;
+
 leftCommand = data.LeftVelocityCommand;
 rightCommand = data.RightVelocityCommand;
 leftVelocity = data.LeftActualVelocity;
 rightVelocity = data.RightActualVelocity;
+
+leftLeadAppliedOutput = data.LeftLeadAppliedOutput;
+leftFollowAppliedOutput = data.LeftFollowAppliedOutput;
+rightLeadAppliedOutput = data.RightLeadAppliedOutput;
+rightFollowAppliedOutput = data.RightFollowAppliedOutput;
+
 leftLeadVoltage = data.LeftLeadVoltage;
 leftFollowVoltage = data.LeftFollowVoltage;
 rightLeadVoltage = data.RightLeadVoltage;
 rightFollowVoltage = data.RightFollowVoltage;
+
+leftLeadOutputVoltage = leftLeadAppliedOutput .* leftLeadVoltage;
+leftFollowOutputVoltage = leftFollowAppliedOutput .* leftFollowVoltage;
+rightLeadOutputVoltage = rightLeadAppliedOutput .* rightLeadVoltage;
+rightFollowOutputVoltage = rightFollowAppliedOutput .* rightFollowVoltage;
+
 leftLeadCurrent = data.LeftLeadCurrent;
 leftFollowCurrent = data.LeftFollowCurrent;
 rightLeadCurrent = data.RightLeadCurrent;
@@ -19,7 +32,7 @@ rightFollowCurrent = data.RightFollowCurrent;
 %% Plot left velocities, voltages, currents
 f1=figure; hold on; grid on;
 
-ax1 = subplot(3,1,1);
+ax1 = subplot(4,1,1);
 grid on;
 hold on;
 
@@ -29,7 +42,7 @@ set(p111,'LineStyle','--');
 set(p111,'Color','r');
 
 %p112 = stairs(time, leftVelocity);
-p112 = stairs(time, leftVelocity/500);
+p112 = stairs(time, leftVelocity);
 set(p112,'LineWidth',2);
 set(p112,'LineStyle','-');
 set(p111,'Color','b');
@@ -39,7 +52,7 @@ ylabel('Velocity [insert units]');
 title('Left Actual and Command Velocity');
 legend('Left Command Velocity','Left Actual Velocity');
 
-ax2 = subplot(3,1,2);
+ax2 = subplot(4,1,4);
 grid on;
 hold on;
 p121 = stairs(time, leftLeadVoltage);
@@ -52,10 +65,10 @@ set(p122,'LineStyle','-');
 set(p122,'Color','b');
 xlabel('Time [s]');
 ylabel('Voltage [Volts]');
-title('Left Lead and Follow Voltages');
-legend('Left Lead Voltage','Left Follow Voltage');
+title('Left Lead and Follow Bus Voltages');
+legend('Left Lead Bus Voltage','Left Follow Bus Voltage');
 
-ax3 = subplot(3,1,3);
+ax3 = subplot(4,1,3);
 grid on;
 hold on;
 p131 = stairs(time, leftLeadCurrent);
@@ -71,11 +84,27 @@ ylabel('Current [Amps]');
 title('Left Lead and Follow Current');
 legend('Left Lead Current','Left Follow Current');
 
-linkaxes([ax1 ax2 ax3], 'x');
+ax4 = subplot(4,1,2);
+grid on;
+hold on;
+p141 = stairs(time, leftLeadOutputVoltage);
+set(p131,'LineWidth',1);
+set(p131,'LineStyle','-');
+set(p131,'Color','r');
+p142 = stairs(time, leftFollowOutputVoltage);
+set(p142,'LineWidth',1);
+set(p142,'LineStyle','-');
+set(p142,'Color','b');
+xlabel('Time [s]');
+ylabel('Applied Output');
+title('Left Lead and Follow Voltage');
+legend('Left Lead Voltage','Left Follow Voltage');
+
+linkaxes([ax1 ax2 ax3 ax4], 'x');
 %% Plot right velocities, voltages, currents
 f2 = figure; hold on; grid on;
 
-ax1 = subplot(3,1,1);
+ax1 = subplot(4,1,1);
 grid on;
 hold on;
 p211 = stairs(time, rightCommand);
@@ -91,7 +120,7 @@ ylabel('Velocity [insert units]');
 title('Right Actual and Command Velocity');
 legend('Right Command Velocity','Right Actual Velocity');
 
-ax2 = subplot(3,1,2);
+ax2 = subplot(4,1,4);
 grid on;
 hold on;
 p221 = stairs(time, rightLeadVoltage);
@@ -104,10 +133,10 @@ set(p222,'LineStyle','-');
 set(p222,'Color','b');
 xlabel('Time [s]');
 ylabel('Voltage [Volts]');
-title('Right Lead and Follow Voltages');
-legend('Right Lead Voltage','Right Follow Voltage');
+title('Right Lead and Follow Bus Voltages');
+legend('Right Lead Bus Voltage','Right Follow Bus Voltage');
 
-ax3 = subplot(3,1,3);
+ax3 = subplot(4,1,3);
 grid on;
 hold on;
 p231 = stairs(time, rightLeadCurrent);
@@ -123,7 +152,23 @@ ylabel('Current [Amps]');
 title('Right Lead and Follow Current');
 legend('Right Lead Current','Right Follow Current');
 
-linkaxes([ax1 ax2 ax3], 'x');
+ax4 = subplot(4,1,2);
+grid on;
+hold on;
+p141 = stairs(time, rightLeadOutputVoltage);
+set(p131,'LineWidth',1);
+set(p131,'LineStyle','-');
+set(p131,'Color','r');
+p142 = stairs(time, rightFollowOutputVoltage);
+set(p142,'LineWidth',1);
+set(p142,'LineStyle','-');
+set(p142,'Color','b');
+xlabel('Time [s]');
+ylabel('Applied Output');
+title('Right Lead and Follow Voltage');
+legend('Right Lead Voltage','Right Follow Voltage');
+
+linkaxes([ax1 ax2 ax3 ax4], 'x');
 
 
 
